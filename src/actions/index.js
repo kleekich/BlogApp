f53667f1c3dacc2f0147c470ce6278d95f424a37
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-03-04 22:57:42 -0800

    spaces deleted

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,5 @@
 import axios from 'axios';
 
-
-
-
-
 export const FETCH_POSTS = 'fetch_posts';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -14,12 +10,9 @@ export function fetchPosts() {
 
 	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
-
-
 	return {
 		type: FETCH_POSTS,
 		payload: request
-
 	};
 
 }

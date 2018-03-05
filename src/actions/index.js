183799b49ea393b0bf630ee6e8266cd5cebbe7ca
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-03-04 22:56:49 -0800

    Making axios request and assign it to payload that action returns

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 
 
 
@@ -5,11 +6,20 @@
 
 export const FETCH_POSTS = 'fetch_posts';
 
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=kleekich'
 
 //action creator
 export function fetchPosts() {
+
+	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+
+
+
 	return {
-		type: FETCH_POSTS
+		type: FETCH_POSTS,
+		payload: request
+
 	};
 
 }

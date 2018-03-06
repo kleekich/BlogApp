00cccd8e765c19e7bcaf2d54bd3c95681ba8e5b6
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-03-06 15:25:13 -0800

    posts_new boilerplate is implemented

---
+++ src/components/posts_new.js
@@ -0,0 +1,20 @@
+import React. {Component} from 'react';
+
+
+
+class PostsNew extends Component {
+
+	render() {
+
+		return (
+			<div>
+				PostsNew!
+			</div>
+
+
+		);
+
+	}
+}
+
+export default PostsNew;

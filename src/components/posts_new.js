50544d0ce0089c2f009d6bdff17404f2308273e3
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-03-08 16:00:19 -0800

    Field component attribute implemented with field wired up

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -4,13 +4,32 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
 
+
+	renderTitleField(field) {
+		return (
+
+			<div>
+				<input
+				/*
+				onChange={field.input.onChange}
+				onFocus={field.input.onFocus}
+				onBlur= {field.input.onBlur}
+				*/
+					{...field.input}
+				}
+				/>
+			</div>
+
+		);
+
+	}
 	render() {
 
 		return (
 			<form>
 				<Field
 					name="title"
-					component={}
+					component={this.renderTitleField}
 				/>
 			</form>
 

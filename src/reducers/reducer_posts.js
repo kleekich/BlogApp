784f0df317421eb0adbc6edf7b3f771a0e4102b3
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-03-08 22:26:32 -0800

    reducer FETCH_POST case added

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
+
 
 
 
@@ -9,7 +10,13 @@ export default function(state = {}, action) {
 
 		case FETCH_POSTS:
 			return _.mapKeys(action.payload.data, 'id');
-			
+		case FETCH_POST:
+			// const post = action.payload.data;
+			// const newState =  { ...state,  };
+			// newState[post.id] = post;
+			// return newState;
+
+			return { ...state, [action.payload.data.id]: action.payload.data }
 		default:
 			return state;
 	}

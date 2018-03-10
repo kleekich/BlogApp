37721cd8c4d672a660b58ea56a7e2cc1fb63278b
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-03-09 21:49:49 -0800

    Link to go back to the list added

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,15 +1,19 @@
 import React , { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPost } from '../actions';
 
 class PostsShow extends Component{
+	//it is called automatically once the component was render to the DOM.
 	componentDidMount() {
-		//We need to get id by accessing URL
+		//We need to get id by accessing URL/provided by react-router
 		const { id } = this.props.match.params;
 		this.props.fetchPost(id);
 	}
 
 	render() {
+		//this.props === ownProps
+		
 		const { post } = this.props;
 
 		if(!post) {
@@ -18,15 +22,18 @@ class PostsShow extends Component{
 
 		return(
 			<div>
+				<Link to="/">Back to List</Link>
 				<h3>{post.title}</h3>
 				<h6>Categories: {post.categories} </h6>
 				<p>{post.content}</p>
+				<button className="btn btn-danger">Delete</button>
 			</div>
 		);
 	}
 }
-
+//ownProps is prop's object that is going to component above.
 function mapStateToProps( { posts }, ownProps){
+	//We want to pass only single post that we are interested in
 	return { post: posts[ownProps.match.params.id]};
 
 }

3a130da07c3c0ba0331e873788a90a7a5c7ca7bc
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-03-09 22:33:04 -0800

    reducer updated for delete post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 
 
@@ -7,7 +7,8 @@ import { FETCH_POSTS, FETCH_POST } from '../actions';
 export default function(state = {}, action) {
 
 	switch (action.type){
-
+		case DELETE_POST:
+			return _.omit(state, action.payload);
 		case FETCH_POSTS:
 			return _.mapKeys(action.payload.data, 'id');
 		case FETCH_POST:
